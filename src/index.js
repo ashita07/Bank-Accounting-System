a8c259428f76464fee7cd9b2c8467193fbe9934f
Ashita <ashitam2407@gmail.com>
2025-03-05 10:45:19 +0530

    added components to redux

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import "./index.css";
 import App from "./App";
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 250 });
+// console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
